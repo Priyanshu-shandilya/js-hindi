6be67b333c3d35adfbbd32fa612fec7ad4a72e86
Priyanshu Shandilya <priyanshushandilya7@gmail.com>
2025-07-08 06:52:57 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId="12345"
+let accountEmail="abc@gmail.com"
+var accountPassword="123"
+accountCity="Deoghar"
+
+ accountEmail="abg@gmail.com"
+accountPassword="12323"
+accountCity="Bengaluru"
+
+// accountId=2
+console.log(accountId);
+
+
+/* Not to use var 
+because of issue of block of scope and functional scope*/
+console.table([accountId, accountEmail, accountPassword, accountCity])
