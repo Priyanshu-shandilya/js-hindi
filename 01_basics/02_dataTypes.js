a1de1479fdfcc5dca22ba1f25e557ad667863bd1
Priyanshu Shandilya <priyanshushandilya7@gmail.com>
2025-07-08 06:51:16 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use Strict "; //treat all Js Code as newer version
+// alert( 3 +3) We are using node Js not browser
+
+console.log( 3  +3)
+//Code readiblity should be high
+
+console.log("Priyanshu")
+
+let name="Priyanshu"
+let age=18
+let isLoggedIn= false
+let state;
+
+//string=""
+//boolean=true/false
+//bigint
+//null=>stand alone value 
+//undefined=>
+    //symbol=>unique
+
+
+//object
+
+
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
