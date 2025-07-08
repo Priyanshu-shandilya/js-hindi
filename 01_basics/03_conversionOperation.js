16b25ef36315e6a0b83a51e1ca3e67535c168550
Priyanshu Shandilya <priyanshushandilya7@gmail.com>
2025-07-08 11:16:07 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score=true
+
+//console.log(typeof score);  //it will return number and it inserted in double inverted commas then it willl be decleared as string.
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//"33"=>33
+//"33abc"=>NaN
+//true=>1,false=>0
+let isLoggedIn=1
+let booleanisLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+//1=>true
+//0=>false
+//""=>false
+//Priyanshu=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
